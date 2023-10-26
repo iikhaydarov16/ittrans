ab593812cbcb48db324a1a1c9181f4344c1209e6
haidarovii <iikhaydarov16@gmail.com>
2023-10-26 15:44:57 +0300

    Первый коммит

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
+$APPLICATION->SetTitle("ittrans");
 ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
